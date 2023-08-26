98e76cb36c9269439f4d70cac5a5f21e2936630d
kleahaka <142035680+kleahaka@users.noreply.github.com>
2023-08-26 16:24:51 +0200

    Add files via upload

---
+++ codeConventer.js
@@ -0,0 +1,11 @@
+//degree in Kalvin
+const kelvin = 0;
+//convert in celsius
+var celsius = kelvin - 273;
+//conver in fahrenheit
+//rounded value in fahrenheit
+let fahrenheit = celsius * (9 / 5) + 32;
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+var newton = celsius * (33 / 100);
+console.log(newton);
